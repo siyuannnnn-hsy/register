7f47b3ececd19d192c087b3aa8b69792fc23819b
siyuannnnn-hsy <1587935941@qq.com>
2020-03-14 16:24:35 +0800

    master

---
+++ index.js
@@ -0,0 +1,64 @@
+$(function () {
+    var $name = $('#name'),
+        $tel = $('#tel'),
+        $pwd = $('#pwd'),
+        $yan = $('#yan'),
+        $namemsg = $('#namemsg'),
+        $telmsg = $('#telmsg'),
+        $pwdmsg = $('#pwdmsg'),
+        $yanmsg = $('#yanmsg'),
+        $yan2 = $('#yan2'),
+        $btn = $('#btn');
+
+    $name.focusout(function () {
+        if($name.val()===''){
+            $namemsg.html('用户名不能为空')
+            $name.select();
+            return false;
+        }
+        if(!/^[a-zA-Z0-9_\u4e00-\u9fa5]+[^\d]+$/.test($name.val())){
+            $namemsg.html('用户名仅支持中英文、数字、下划线且不能为纯数字')
+            $name.select();
+            return false;
+        }
+    })
+
+   $tel.focusout(function(){
+       console.log($tel.val().length)
+       if($tel.val()===''){
+            $telmsg.html('手机号不能为空')
+            $tel.select();
+            return false;
+       }
+       if(!/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-]?\d+)?$/.test($tel.val())){
+            $telmsg.html('必须是数字')
+            $tel.select();
+            return false;
+       }
+       if($tel.val().length<11){
+            $telmsg.html('手机号格式不正确')
+            $tel.select();
+            return false;
+       }
+    })
+
+    $pwd.focusout(function () {
+        if($pwd.val()===''){
+            $pwdmsg.html('密码不能为空')
+            $pwd.select();
+            return false;
+        }
+        if(!/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{4,8}$/.test($pwd.val())){
+            $pwdmsg.html('密码长度要4-8位，由数字和字母组成')
+            $pwd.select();
+            return false;
+        }
+    })
+
+    $yan2.click(function(){
+        setTimeout(function()  {
+            console.log('11')
+            $yanmsg.html('请求超时，请稍后再试')
+        }, 1000);
+    })
+})
